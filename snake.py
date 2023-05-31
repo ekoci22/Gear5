3073855c5b8281046fa88e01407d4b86cdc71250
Ersido Dingo <edingo22@epoka,edu.al>
2023-06-01 00:08:56 +0200

    Food

--- snake.py
+++ snake.py
@@ -16,11 +16,29 @@ BACKGROUND_COLOR="#000000"
 
 
 class Snake:
-    pass
+       def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = Canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 
 class Food:
-    pass
+    
+
+     def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+        Canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 
 def next_turn():
@@ -61,5 +79,13 @@ x=int((screen_width/2)-(window_width/2))
 y=int((screen_height/2)-(window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake=Snake()
+food=Food()
+
+
+
+
+
 
 window.mainloop()
