3c875c7587a73f71e26b59784a3a18d13d4cac7d
Erdi Koci <erdikoci97@gmail.com>
2023-05-31 22:51:34 +0200

    Bacground

--- snake.py
+++ snake.py
@@ -8,8 +8,8 @@ SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "FF0000"
-BACKGROUND_COLOR="000000"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR="#000000"
 
 
 
@@ -47,6 +47,8 @@ score = 0
 direction='down'
 label= Label(window, text="Score: {}".format(score), font=('consolas',40))
 label.pack()
+Canvas=Canvas(window,bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
+Canvas.pack()
 
 
 window.mainloop()
