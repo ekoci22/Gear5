92335ffd6fde20dc03d288b0ba7366b4555d2b08
Kevi Koro <kkoro22@epoka.edu.al>
2023-06-01 20:59:37 +0200

    Finish

--- snake.py
+++ snake.py
@@ -4,19 +4,14 @@ import random
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
-SPEED = 50
+SPEED = 100
 SPACE_SIZE = 50
-BODY_PARTS = 10
+BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
-BACKGROUND_COLOR="#000000"
-
-
-
-
+BACKGROUND_COLOR = "#000000"
 
 class Snake:
-
     def __init__(self):
         self.body_size = BODY_PARTS
         self.coordinates = []
@@ -31,9 +26,7 @@ class Snake:
 
 
 class Food:
-
     def __init__(self):
-
         x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
         y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
 
@@ -43,7 +36,6 @@ class Food:
 
 
 def next_turn(snake, food):
-
     x, y = snake.coordinates[0]
 
     if direction == "up":
@@ -62,34 +54,23 @@ def next_turn(snake, food):
     snake.squares.insert(0, square)
 
     if x == food.coordinates[0] and y == food.coordinates[1]:
-
         global score
-
         score += 1
-
         label.config(text="Score:{}".format(score))
-
         canvas.delete("food")
-
         food = Food()
-
     else:
-
         del snake.coordinates[-1]
-
         canvas.delete(snake.squares[-1])
-
         del snake.squares[-1]
 
     if check_collisions(snake):
         game_over()
-
     else:
         window.after(SPEED, next_turn, snake, food)
 
 
 def change_direction(new_direction):
-
     global direction
 
     if new_direction == 'left':
@@ -107,7 +88,6 @@ def change_direction(new_direction):
 
 
 def check_collisions(snake):
-
     x, y = snake.coordinates[0]
 
     if x < 0 or x >= GAME_WIDTH:
@@ -123,10 +103,22 @@ def check_collisions(snake):
 
 
 def game_over():
-
     canvas.delete(ALL)
     canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
-                       font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
+                       font=('consolas', 70), text="GAME OVER", fill="red", tag="gameover")
+    start_button.config(state=NORMAL)
+
+
+def start_game():
+    global snake, food, score, direction
+    score = 0
+    direction = 'down'
+    label.config(text="Score:{}".format(score))
+    canvas.delete(ALL)
+    start_button.config(state=DISABLED)
+    snake = Snake()
+    food = Food()
+    next_turn(snake, food)
 
 
 window = Tk()
@@ -141,6 +133,10 @@ label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
+
+start_button = Button(window, text="Start", font=('consolas', 20), command=start_game)
+start_button.pack()
+start_button.focus_set()
 
 window.update()
 
@@ -159,9 +155,7 @@ window.bind('<Right>', lambda event: change_direction('right'))
 window.bind('<Up>', lambda event: change_direction('up'))
 window.bind('<Down>', lambda event: change_direction('down'))
 
-snake = Snake()
-food = Food()
-
-next_turn(snake, food)
+snake = None
+food = None
 
 window.mainloop()
